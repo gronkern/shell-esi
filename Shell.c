f5e8b217f9db3042ea09908480e42f6fa189a862
Placentino Simon <39631@heb.be>
2015-05-16 16:27:29 +0200

    clean

--- Shell.c
+++ Shell.c
@@ -19,89 +19,97 @@ void shellPrompt()
 	printf("[Shell]:%s$ ", getcwd(NULL, 0));
 }
 
-int parsecmd(char * cmd, char ** args, int * bg, int * out) {
-	char * c;
-	DIR * current;
-	struct dirent * dir;
-	char * name[100];
-	int j =0;
+int parsecmd(char * cmd, char ** args, int * bg, int * out)
+{
 	int i = split(args, cmd, " \n");
 
-	if (i > 0) {
-		if (strcmp(args[i-1], "*") == 0) {
-			current = opendir(".");
-			while ((dir = readdir(current)) != NULL) {
-				if ((strcmp(dir->d_name, "..") != 0) && (strcmp(dir->d_name, ".") != 0)) {
+	if (i > 0) // Paramètre *
+	{
+		if (strcmp(args[i - 1], "*") == 0) 
+		{
+			struct dirent * dir;
+			DIR * current = opendir(".");
+			int j = 0;
+			char * folders[100]; // Maximum 100 directories.
 
-					name[j] = malloc(sizeof(char) *50);
-					strcpy(name[j], dir->d_name);
-					args[(i++)-1] = name[j++];
+			while ((dir = readdir(current)) != NULL) 
+			{
+				if ((strcmp(dir->d_name, "..") != 0) 
+						&& (strcmp(dir->d_name, ".") != 0)) 
+				{
+					folders[j] = malloc(sizeof(char) * 50); // Taille 50 char
+					strcpy(folders[j], dir->d_name);
+					args[(i++) - 1] = folders[j++];
 				}
 			}
-			args[(i--)-1] = NULL;
+
+			args[(i--) - 1] = NULL;
 			closedir(current);
-
-
 		}
 	}
-	   if (i > 0) {	
 
-	   c = strrchr(args[i-1], '&');
+	if (i > 0) // Programme en background
+	{ 
+		char * c = strrchr(args[i - 1], '&');
 
+		if (strcmp(args[i - 1], "&") == 0) 
+		{
+			*bg = 1;
+			args[i - 1] = NULL;
+		} 
+		else if (c != NULL) 
+		{
+			*bg = 1;
+			*c = '\0';
+		}			
+	}
 
-	   if (strcmp(args[i-1], "&") == 0) {
-	 *bg = 1;
-	 args[i-1] = NULL;
-
-	 } else if (c != NULL) {
-	 *bg = 1;
-	 *c = '\0';
-	 }			
-	 }
-	   if (i > 1) {
-	   if (strcmp(args[i-2], ">") == 0) {
-	 *out = 1;	
-	 }
-
-	 }
+	if (i > 1) // Redirection
+	{ 
+		if (strcmp(args[i-2], ">") == 0)
+			*out = 1;	
+	}
 
 	return i;
 }
 
 int execcmd(char ** args, int * bg, int * out, int i) {
 
-	int h;
-	int pid = fork();
 	int j;
-	// ERREUR
-	if (pid < 0) 
+	int pid;
+
+	if ((pid = fork()) < 0) 
 		printf("Erreur fork : %s\n", strerror(errno));
 
 	// FILS
-	else if (pid == 0) {
-
+	else if (pid == 0)
+	{
 		//redirection
-		   if (*out == 1) {
-		   h = open(args[i-1], O_WRONLY | O_CREAT | O_TRUNC, 0644);	
-		   dup2(h, 1);
-		   close(h);
-		   args[i-2] = NULL;
-		   }	
+		if (*out == 1)
+		{
+			int h = open(args[i - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);	
+
+			dup2(h, 1);
+
+			close(h);
+			args[i-2] = NULL;
+		}	
 
 		if (execvp(args[0], args) != 0)
 			printf("Commande introuvable\n");
 
-
-		return 0;
-
-		//PERE
-	} else {
-		if (*bg == 0) {
+		exit(EXIT_SUCCESS);
+	} 
+	else 
+	{
+		if (*bg == 0) 
 			waitpid(pid, 0, 0);
-		} else {
+		else 
+		{
 			bgcpt++;
 			printf("[%d] %d\n", bgcpt, pid); 
 		}
+
 		return 1;
 	}
 
@@ -112,16 +120,14 @@ int main(int argc, char * argv[]) {
 
 	char buffer[300];
 	char * args[100];
-	int bg;
-	int out;
-	int i;
-	while (TRUE) {
-		bg = 0;
-		out = 0;
+	while (TRUE) 
+	{
 		shellPrompt();
-		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+
+		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+		{
 			printf("\n");
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 
 		/* LIGNE VIDE */
@@ -129,29 +135,29 @@ int main(int argc, char * argv[]) {
 			continue;
 
 		/* EXIT */
-		if (strcmp(buffer, "exit\n") == 0) {
+		if (strcmp(buffer, "exit\n") == 0)
+		{
 			printf("Goodbye ! \n");
-			exit(0);
+			exit(EXIT_SUCCESS);
 		}
 
-
-
 		/* PARSING CMD */
-		i = parsecmd(buffer , args, &bg, &out);
+		int bg = 0;
+		int out = 0;
+		int i = parsecmd(buffer , args, &bg, &out);
 
-
-		if (strcmp(args[0], "cd") == 0) {
+		if (strcmp(args[0], "cd") == 0)
+		{
 			if (args[1] == NULL) 
 				chdir(getenv("HOME"));
 			else if (chdir(args[1]) != 0) 
 				printf("cd : no such files or directory\n");
-
-		} else {
-
+		} 
+		else 
+		{
 			/* EXEC CMD */
 			if (execcmd(args, &bg, &out, i) == 0)
-				exit(0);
-
+				exit(EXIT_SUCCESS);
 		}
 	}
 
