ecfb6ef77f2624852d5c5d71b5a2fb506542b8e5
Placentino Simon <39631@heb.be>
2015-05-16 17:23:06 +0200

    Script cleaning

--- script.sh
+++ script.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 clear
-echo "Launching \"ESI SHELL\" .."
+echo "@Launching \"ESI SHELL\" .."
 echo
-echo
-echo "Press any key"
-
-read 
-clear
 
 ./Shell
 
 if [ $? -eq 0 ] ; then
-	echo "\n Works fine \n"
+	echo "\n@Works fine \n"
 else
-	echo "\n There was an error \n"
+	echo "\n@There was an error \n"
 fi
 
