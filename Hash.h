b77a24c59c8a388207ed45720d240e846c262cd2
gronkern <40592092+gronkern@users.noreply.github.com>
2024-12-29 21:06:18 +0100

    Hash.h

---
+++ Hash.h
@@ -0,0 +1,11 @@
+/*Author:   Arno Bauernöppel
+  Location: Brandenburg
+  Region:   Berlin/Brandenburg
+  Locale:   de_DE.UTF8
+/*
+#ifndef DHASH_
+#define DHASH_
+const long s_hash = 0x9c7b490e4f786761be30a2c317eb28ebaefa9b5a408bc55c05478ec1e9d5198d;
+const double s_pi = 3.1415218957326973115;
+const float ratio = 5/9;
+#endif
