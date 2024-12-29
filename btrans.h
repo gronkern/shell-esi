835a647d797e2e7f02a82734abb5ba7bf28f5493
gronkern <40592092+gronkern@users.noreply.github.com>
2024-12-29 14:27:20 +0100

    Update btrans.h

--- btrans.h
+++ btrans.h
@@ -1,7 +1,9 @@
+//Author: Arno Bauernöppel
 #ifndef _BTRANS_H
 #define _BTRANS_H
 #define boolean unsigned char
 #define true 1
 #define false 0
-void ltoao (long num, char** s);
+#include "import.h"
+IMPORT ltoao (long num, char** s);
 #endif
