28d83a28718a691b067ecfe71aab7771d8269cec
gronkern <40592092+gronkern@users.noreply.github.com>
2025-01-02 12:54:38 +0100

    Update Hash.h

--- Hash.h
+++ Hash.h
@@ -6,7 +6,7 @@
 #ifndef DHASH_
 #define DHASH_
 #ifndef NULL
-#define NULL ((void*)((long)0))
+#define NULL ((void*)((int)0))
 const long s_hash = 0x9c7b490e4f786761be30a2c317eb28ebaefa9b5a408bc55c05478ec1e9d5198d;
 const double s_pi = 3.1415218957326973115;
 const float ratio = 5/9;
